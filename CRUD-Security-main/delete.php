38bd168a134180036ec966d1866ff8c57a2df32e
Ritesh Kumar Jena <149995969+riteshkumarjena@users.noreply.github.com>
2025-09-10 20:28:04 +0530

    Update delete.php

--- CRUD-Security-main/delete.php
+++ CRUD-Security-main/delete.php
@@ -52,6 +52,8 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
     <button type="submit" name="confirm" class="confirm-delete">Yes, Delete</button>
     <a href="dashboard.php" class="cancel-btn">Cancel</a>
   </form>
+    <!-- Developed by @Ritesh Kumar Jena -->
 </div>
 </body>
+
 </html>
